8c8d1c379c91e42e8ced3132f1667e658f00b960
Artem.Gunin <artemgunin1993@gmail.com>
2022-08-13 23:59:59 +0300

    Module 2

--- GuninProjekt/src/main/java/module02/src/main/java/com/Main.java
+++ GuninProjekt/src/main/java/module02/src/main/java/com/Main.java
@@ -1,30 +1,12 @@
 package com;
 
 import com.command.ConsoleMenu;
-import com.repository.InvoiceRepository;
-import com.service.AnalyticalUtils;
-import com.service.ShopService;
 
 import java.io.IOException;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
-
-        ShopService shopService = new ShopService(InvoiceRepository.getInstance());
-
         ConsoleMenu.menu();
-
-        //System.out.println(shopService.readFile("products.csv"));
-
-//        shopService.readFile("products.csv")
-//                .forEach(System.out::println);
-//        System.out.println(ProductType.valueOf("Telephone".toUpperCase()));
-
-
-
-//        shopService.createAndSaveInvoices(15,"products.csv", 1000);
-//        AnalyticalUtils analyticalUtils = new AnalyticalUtils(InvoiceRepository.getInstance());
-//        analyticalUtils.printAnalyticalData();
     }
 }
