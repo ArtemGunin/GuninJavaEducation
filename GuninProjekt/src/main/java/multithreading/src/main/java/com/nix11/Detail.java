bf9aa37c7854a09b5a51aebbb0b5301b7b5ba008
Artem.Gunin <artemgunin1993@gmail.com>
2022-09-19 03:08:19 +0300

    Lesson 30. Multithreading.

---
+++ GuninProjekt/src/main/java/multithreading/src/main/java/com/nix11/Detail.java
@@ -0,0 +1,13 @@
+package com.nix11;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Detail {
+    private int detail = 0;
+    private int basicPartDesign = 0;
+    private int chipProgramming = 0;
+    private int partFormation = 0;
+}
